2818c4551097f5e0bbb2a633bee419046c2985c5
Muhammad Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2021-05-06 04:52:38 +0500

    Practcing Webpack with React

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
-var webpack = require('webpack');
-var path = require('path');
+let webpack = require('webpack');
+let path = require('path');
+let HtmlWebPackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: ['react','lodash','redux','react-redux','react-dom','faker','react-input-range','redux-form','redux-thunk']
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name][chunkhash].js'
+  },
+  module:{
+    rules:[
+      {
+        use:'babel-loader', 
+        test: /\.js$/,
+        exclude: /node_modules/ 
+      }
+      ,{
+        use: ['style-loader','css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins:[
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor','manifest']
+    }),
+    new HtmlWebPackPlugin({
+      template: 'index.html'
+    })
+  ]
 };
